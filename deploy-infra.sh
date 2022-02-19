9aac2d0f33e10cf3e81330d69dd0637adaf0c78e
aleksandarMitrovic446 <aleksa446@gmail.com>
2022-02-19 21:51:31 +0100

    Added buildspec.yml and appspec.yml

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -3,15 +3,29 @@
 STACK_NAME=awsbootstrap
 REGION=eu-central-1
 CLI_PROFILE=aleksa446-admin
-
 EC2_INSTANCE_TYPE=t2.micro
+
+# Get AWS account ID
+AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile $CLI_PROFILE --query "Account" --output text`
+CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
+
+# Deploys static resources (S3)
+echo -e "\n\n=========== Deploying setup.yml ==========="
+aws cloudformation deploy \
+  --region $REGION \
+  --profile $CLI_PROFILE \
+  --stack-name $STACK_NAME-setup \
+  --template-file templates/setup.yml \
+  --no-fail-on-empty-changeset \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameter-overrides CodePipelineBucket=$CODEPIPELINE_BUCKET
 
 echo -e "\n\n======= Deploying main.yml ============"
 aws cloudformation deploy \
   --region $REGION \
   --profile $CLI_PROFILE \
   --stack-name $STACK_NAME \
-  --template-file main.yml \
+  --template-file templates/main.yml \
   --no-fail-on-empty-changeset \
   --capabilities CAPABILITY_NAMED_IAM \
   --parameter-overrides EC2InstanceType=$EC2_INSTANCE_TYPE
