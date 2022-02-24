69687f1710e30e36fa79ebfcd65c813833c5a449
aleksandarMitrovic446 <aleksa446@gmail.com>
2022-02-24 23:11:24 +0100

    Added hostname to gello world message

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -45,5 +45,5 @@ aws cloudformation deploy \
 if [ $? -eq 0 ]; then
   aws cloudformation list-exports \
     --profile $CLI_PROFILE \
-    --query "Exports[?Name=='InstanceEndpoint'].Value"
+    --query "Exports[?starts_with(Name,'InstanceEndpoint')].Value"
 fi
